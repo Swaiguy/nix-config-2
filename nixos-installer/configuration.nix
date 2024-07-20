df431607ea1d78dab662c65e40bdb383fb9933a3
Swaiguy <126988847+Swaiguy@users.noreply.github.com>
2024-07-20 16:23:11 +0000

    Add files via upload

---
+++ nixos-installer/configuration.nix
@@ -0,0 +1,25 @@
+{pkgs, ...}: {
+  # ssh-agent is used to pull my private secrets repo from github when deploying my nixos config.
+  programs.ssh.startAgent = true;
+
+   services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "kry";
+  users.users.kry.password ="123";
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    git
+    gnumake
+    wget
+    just # a command runner(replacement of gnumake in some cases)
+    curl
+    nix-output-monitor
+  ];
+  networking = {
+    # configures the network interface(include wireless) via `nmcli` & `nmtui`
+    networkmanager.enable = true;
+    defaultGateway = "192.168.18.1";
+  };
+  system.stateVersion = "24.05";
+}
