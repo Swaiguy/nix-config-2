ecc86b3a9d4c1f0626cc0a042caa63f42b84a8a4
Swaiguy <126988847+Swaiguy@users.noreply.github.com>
2024-07-21 06:19:49 +0000

    Update configuration.nix

--- hosts/idols-ai/nixos-installer/configuration.nix
+++ hosts/idols-ai/nixos-installer/configuration.nix
@@ -27,6 +27,7 @@
   };
 
   # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.wayland.enable
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
