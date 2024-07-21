1043a3e3ffea3dbb9e537c1b8874d42e35fd5241
Swaiguy <126988847+Swaiguy@users.noreply.github.com>
2024-07-21 05:27:54 +0000

    Update base.nix

--- modules/base.nix
+++ modules/base.nix
@@ -59,6 +59,27 @@
     rsync
   ];
 
+  users.users.${myvars.username} = {
+    description = myvars.userfullname;
+    # Public Keys that can be used to login to all my PCs, Macbooks, and servers.
+    #
+    # Since its authority is so large, we must strengthen its security:
+    # 1. The corresponding private key must be:
+    #    1. Generated locally on every trusted client via:
+    #      ```bash
+    #      # KDF: bcrypt with 256 rounds, takes 2s on Apple M2):
+    #      # Passphrase: digits + letters + symbols, 12+ chars
+    #      ssh-keygen -t ed25519 -a 256 -C "ryan@xxx" -f ~/.ssh/xxx`
+    #      ```
+    #    2. Never leave the device and never sent over the network.
+    # 2. Or just use hardware security keys like Yubikey/CanoKey.
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIKlN+Q/GxvwxDX/OAjJHaNFEznEN4Tw4E4TwqQu/eD6 ryan@idols-ai"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPoa9uEI/gR5+klqTQwvCgD6CD5vT5iD9YCNx2xNrH3B ryan@fern"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPwZ9MdotnyhxIJrI4gmVshExHiZOx+FGFhcW7BaYkfR ryan@harmonica"
+    ];
+  }
+
   nix.settings = {
     # enable flakes globally
     experimental-features = ["nix-command" "flakes"];
